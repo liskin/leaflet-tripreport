3f4bbfd7e89501c8ade6a782105aa23d43cfce2d
Tomas Janousek <tomi@nomi.cz>
2017-10-01 19:51:47 +0200

    Prepare compact JSON beforehand
    Also, fix photo locations using timestamps and GPX tracks.

--- map.js
+++ map.js
@@ -44,61 +44,14 @@
 	});
 	L.control.layers(baseMaps, overlayMaps).addTo(map);
 
-	/*
-	In browser that's logged in to Google:
-	- create new tmp album
-	- https://picasaweb.google.com/data/feed/api/user/113411333440293111262
-	- find album
-	- https://picasaweb.google.com/data/feed/api/user/113411333440293111262/albumid/6471621605834696945
-	- save as photos.xml
-	*/
-	$.get('photos.xml', null, null, 'xml').then(function (data) {
-		var photos = $(data).children('feed').children('entry').map(function () {
-			var photo = this;
-			var pos = $(photo).find('gml\\:pos').text().split(' ');
-			if (pos.length != 2) {
-				return [];
-			}
-			// TODO: brát ty pozice z gpx
-
-			var thumbnail = $(photo).find('media\\:thumbnail[width=72],media\\:thumbnail[height=72]').attr('url');
-			var preview = $(photo).find('media\\:thumbnail[width=288],media\\:thumbnail[height=288]').attr('url');
-			var url = preview.replace('/s288/', '/s0/');
-
-			return {
-				lat: pos[0],
-				lon: pos[1],
-				thumbnail: thumbnail,
-				preview: preview,
-				url: url,
-			};
-		}).get();
-		layerPhoto.add(photos);
+	$.get('data.json', function (data) {
+		layerPhoto.add(data.photos);
+		data.tracks.forEach(function (track) {
+			layerGpx.addLayer(L.polyline(track.coords, {
+				weight: 5,
+				color: track.color,
+			}));
+		});
 		map.fitBounds(layerPhoto.getBounds());
-	});
-
-	function parseGpx(gpx) {
-		var trk = $(gpx).children('gpx').children('trk').get(0);
-		var name = $(trk).children('name').text();
-		var trkpts = $(trk).children('trkseg').children('trkpt').get();
-		var coords = trkpts.map(function (trkpt) {
-			return [
-				1 * trkpt.getAttribute('lat'),
-				1 * trkpt.getAttribute('lon'),
-			];
-		});
-
-		return L.polyline(coords, {
-			color: name.match('spaní') ? '#0000ff' : '#ff0000',
-			weight: 5,
-		});
-
-		// TODO: link na stravu
-	}
-
-	['den1a.gpx', 'den1b.gpx', 'den2a.gpx', 'den2b.gpx', 'den3.gpx'].forEach(function (name) {
-		$.get(name, null, null, 'xml').then(function (data) {
-			layerGpx.addLayer(parseGpx(data));
-		});
 	});
 })();
---
+++ prepare_data.pl
@@ -0,0 +1,115 @@
+#!/usr/bin/perl -CSA
+
+use Modern::Perl qw(2015);
+use autodie;
+use utf8;
+
+use DateTime::Format::ISO8601;
+use JSON::XS;
+use Path::Tiny;
+use XML::Simple;
+
+use Data::Printer;
+
+sub from_xml {
+	return XMLin(shift, ForceArray => 1, KeyAttr => [], KeepRoot => 1);
+}
+
+sub to_json {
+	state $json = JSON::XS->new->canonical(1);
+	$json->encode(shift);
+}
+
+sub main {
+	my ($photos_xml, @tracks_gpx) = @_;
+
+	my @all_locations;
+	my @tracks = map { prepare_track($_, \@all_locations) } @tracks_gpx;
+	my @photos = prepare_photos($photos_xml);
+
+	@all_locations = sort { $a->{timestamp} <=> $b->{timestamp} } @all_locations;
+	@photos = sort { $a->{timestamp} <=> $b->{timestamp} } @photos;
+	fix_photo_locations(\@photos, \@all_locations);
+
+	my $output = {
+		tracks => \@tracks,
+		photos => \@photos,
+	};
+
+	print to_json($output);
+}
+
+# GPX from Strava
+sub prepare_track {
+	my ($filename, $all_locations) = @_;
+
+	my $xml = from_xml(path($filename)->slurp_utf8);
+	my $trk = $xml->{gpx}->[0]->{trk}->[0];
+	my $name = $trk->{name}->[0];
+	my $color = $name =~ /spaní/ ? '#0000ff' : '#ff0000';
+	my @trkpts = map { @{$_->{trkpt}} } @{$trk->{trkseg}};
+	my @coords = map +{
+		lat => 1 * $_->{lat},
+		lon => 1 * $_->{lon},
+		timestamp => iso8601_to_timestamp($_->{time}->[0]),
+	}, @trkpts;
+	push @$all_locations, @coords;
+
+	{
+		name => $name,
+		color => $color,
+		coords => [ map +[ $_->{lat}, $_->{lon} ], @coords ],
+		# TODO: Strava link
+	};
+}
+
+sub iso8601_to_timestamp {
+	DateTime::Format::ISO8601->parse_datetime(shift)->epoch;
+}
+
+# In browser that's logged in to Google:
+# - create new tmp album
+# - https://picasaweb.google.com/data/feed/api/user/113411333440293111262
+# - find album
+# - https://picasaweb.google.com/data/feed/api/user/113411333440293111262/albumid/6471621605834696945
+# - save as photos.xml
+sub prepare_photos {
+	my $filename = shift;
+
+	my $xml = from_xml(path($filename)->slurp_utf8);
+	map { prepare_photo($_) } @{$xml->{feed}->[0]->{entry}};
+}
+
+sub prepare_photo {
+	my $entry = shift;
+
+	my @thumbnails = map { @{$_->{'media:thumbnail'}} } @{$entry->{'media:group'}};
+	my ($thumbnail) = map { $_->{url} } grep { $_->{width} == 72 or $_->{height} == 72 } @thumbnails;
+	my ($preview) = map { $_->{url} } grep { $_->{width} == 288 or $_->{height} == 288 } @thumbnails;
+	my $url = $preview =~ s|/s288/|/s0/|r;
+	my ($lat, $lon) = split(' ', $entry->{'georss:where'}->[0]->{'gml:Point'}->[0]->{'gml:pos'}->[0] // '');
+
+	{
+		timestamp => $entry->{'gphoto:timestamp'}->[0] / 1000,
+		thumbnail => $thumbnail,
+		preview => $preview,
+		url => $url,
+		lat => $lat,
+		lon => $lon,
+	};
+}
+
+sub fix_photo_locations {
+	my ($photos, $all_locations) = @_;
+
+	my $current_location = shift @$all_locations;
+	for my $photo (@$photos) {
+		while ($current_location->{timestamp} < $photo->{timestamp} and @$all_locations) {
+			$current_location = shift @$all_locations;
+		}
+
+		@$photo{'lat', 'lon'} = @$current_location{'lat', 'lon'};
+	}
+}
+
+main(@ARGV);
