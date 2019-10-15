b34cb2fd9632574f9870b3a626c9e79855a183c8
Tomas Janousek <tomi@nomi.cz>
2019-10-15 20:54:59 +0200

    Fix join.sh output to work as join.sh input too

--- join.sh
+++ join.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 perl -Mfeature=say -ne 'm|^tripReport\((.*)\);$| or die; print $1' "$@" \
-| jq -r -s -c '{photos: [(.[].photos // [])[]], points: [(.[].points // [])[]], tracks: [(.[].tracks // [])[]]} | tojson | "tripReport(\(.))"'
+| jq -r -s -c '{photos: [(.[].photos // [])[]], points: [(.[].points // [])[]], tracks: [(.[].tracks // [])[]]} | tojson | "tripReport(\(.));"'
